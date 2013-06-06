d6addea5a7c198465595bf790c8cd4303a0c2e6d
dgageot <david@gageot.net>
2013-06-06 11:11:28 +0200

    Init
    Initial version
    
    Boxen integration
    
    Defaults
    
    Fix git alias
    
    More configuration
    
    More settings
    
    cask
    
    More apps
    
    Alfred and Cask
    
    Default to Java 6
    
    Utilise la branche courante
    
    Corrige tabulations
    
    Fix git commands
    
    Fix scripts if not in git repo
    
    jenv
    
    Fix rebase
    
    Better error messages
    
    Fix jenv config
    
    More apps
    
    git build
    
    More apps
    
    FIX git-build
    
    more apps
    
    FIX git-build
    
    FIX
    
    Ignore osx files
    
    Maven configuration
    
    Install all apps through brew
    
    Fix image path
    
    Uninstall boxen
    
    Install all application through homebrew
    
    Git config
    
    Slate default config
    
    FIX Global ignore
    
    Uninstall moom
    
    Install git
    
    Fix homebrew install after boxen uninstallation
    
    Better homebrew script
    
    Better Slate configuration
    
    Install node
    
    Update Slate config
    
    Add vagrant packages
    
    RVM
    
    Heroku
    
    Launch Intellij
    
    Fix script
    
    Fix script
    
    Textmate alias
    
    Git credentials
    
    Nouvelles applis
    
    Don't use an alias
    
    VLC
    
    Ignore jenv files
    
    FIX
    
    Show all untracked files
    
    Install more applications
    
    Files are easier to edit
    
    Remove apps
    
    Improve installation of brew formulas
    
    Better git status
    
    Buggy formulas
    
    Install jenv
    
    Configure contacts
    
    Fix
    
    fixes
    
    Fix Intellij
    
    VisualVm
    
    Eclipse
    
    More formulas
    
    Doesn't work
    
    gfx
    
    Colored man pages
    
    Git-Crypt
    
    Clean
    
    Git amend
    
    Add gource formula
    
    cdf
    
    More aliases
    
    Docker
    
    Fix notification
    
    More apps
    
    docker on gce
    
    gcutils
    
    Add mongodb
    
    Outdated formulas
    
    typo
    
    CHROME
    
    ngrok
    
    Use gcloud
    
    Another Slate config for BaseCamp
    
    tmux
    
    Docker update
    
    Docker build
    
    Docker url
    
    Not used
    
    Dont fail if files dont exist
    
    Unused formulas
    
    minor
    
    git completions
    
    Fix CDPATH
    
    Update for java 8
    
    Never used it
    
    Fix
    
    Update cask formulas
    
    Add formulas
    
    One more helper script
    
    jenv script
    
    Fix path
    
    docker run alias
    
    docker-ip
    
    docker-enter
    
    More settings
    
    docker rm
    
    Rollback
    
    kube
    
    Better than aliases
    
    Update aliases
    
    Useful for demos
    
    replace mate with atom
    
    unregister/register
    
    textmate to atom migration
    
    Install z
    
    Tmux
    
    Use atom
    
    Install most
    
    Set default vagrant provider
    
    simple prompt
    
    Stable
    
    Fix configuration
    
    Move prompt to its own file
    
    Fix prompt
    
    Dead code
    
    Better tmux config
    
    Tmux attach
    
    Syntax highlight
    
    Default java
    
    Fixes
    
    Fixes
    
    Fixes
    
    fixes
    
    Unused
    
    relative path
    
    Comment
    
    gcloud
    
    VIM
    
    Less
    
    iTerm prefs
    
    Don't ignore bin folder
    
    Better colors
    
    Better colors
    
    iTerm key mapping
    
    Latest Chrome
    
    Global keymap
    
    Tmux conf
    
    Dead code
    
    Show human sizes
    
    Atom is too slow
    
    Also install chrome beta
    
    Minimal vi
    
    Better style for less
    
    vi config
    
    Init

---
+++ bash/aliases.bashrc
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+alias ls='ls -GFlh'
+alias la='ls -a'
+alias ij='open -b com.jetbrains.intellij'
+alias ws='open -b com.jetbrains.webstorm'
+alias cu='open -a Google\ Chrome --args --disable-web-security'
---
+++ osx/set-defaults.sh
@@ -0,0 +1,155 @@
+#!/bin/sh
+#
+# Sets reasonable OS X defaults.
+#
+# Or, in other words, set shit how I like in OS X.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Menu bar: disable transparency
+defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local
+
+# Disable Resume system-wide
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+# Disable automatic termination of inactive apps
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+# Automatically illuminate built-in MacBook keyboard in low light
+defaults write com.apple.BezelServices kDim -bool true
+# Turn off keyboard illumination when computer is not used for 5 minutes
+defaults write com.apple.BezelServices kDimTime -int 300
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Enable spring loading for directories
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+
+# Remove the spring loading delay for directories
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Use column view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle clmv
+
+# Show battery percentage
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Open and save files as UTF-8 in TextEdit
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Open Finder on Home
+defaults write com.apple.finder NewWindowTarget PfHm
+
+###############################################################################
+# SSD-specific tweaks                                                         #
+###############################################################################
+
+# Disable local Time Machine snapshots
+sudo tmutil disablelocal
+
+# Disable hibernation (speeds up entering sleep mode)
+sudo pmset -a hibernatemode 0
+
+# Remove the sleep image file to save disk space
+sudo rm /Private/var/vm/sleepimage
+# Create a zero-byte file instead…
+sudo touch /Private/var/vm/sleepimage
+# …and make sure it can’t be rewritten
+sudo chflags uchg /Private/var/vm/sleepimage
+
+# Disable the sudden motion sensor as it’s not useful for SSDs
+sudo pmset -a sms 0
+
+# Scripts
+# osascript scripts/contacts.scpt > /dev/null
+
+# Restart applications
+killall Finder Contacts
